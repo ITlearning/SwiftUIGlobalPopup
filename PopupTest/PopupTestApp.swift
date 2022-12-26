a50b7cc23e42bd339e873152bacf57fd2bdd6a8f
tabber <tabber@jejememe.com>
2022-12-26 17:59:35 +0900

    Initial Commit

---
+++ PopupTest/PopupTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopupTestApp.swift
+//  PopupTest
+//
+//  Created by Tabber on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct PopupTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
